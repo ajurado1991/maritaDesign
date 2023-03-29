12ed924fdfd982ea10ca16198a330e149ef58f39
alainjurado91 <e.alain.jurado@gmail.com>
2023-03-28 20:07:16 -0600

    commit, preentrega 1 terminada

--- main.js
+++ main.js
@@ -8,28 +8,29 @@ let item4 = "Llavero con ilustraciòn";
 
 if ( userAge >= 18) {
     alert ("Bienvenido " + nameUser +  " puedes realizar compras en el sitio");
+
+    let continuar = confirm (nameUser + " ¿Quieres conocer los artículos de nuestro catálogo?");
+    do {
+        const usuarioSesion = prompt ("Por favor ingresa tu nombre de usuario");
+       const passwordSesion = prompt ("Por favor ingresa tu contraseña");
+   
+       if (usuarioSesion == nameUser && passwordSesion == passwordUser) {
+           alert ("Bienvenido " + nameUser + " por el momento tenemos a la venta los siguientes productos: " + item1 + ", " + item2 + ", "+ item3 + ", "+ item4 + ".");
+       } else if (usuarioSesion != nameUser && passwordSesion == passwordUser) {
+           alert ("Por favor, ingresa un nombre de usuario correcto");
+       } else if (usuarioSesion == nameUser && passwordSesion != passwordUser){
+           alert("Por favor, ingresa tu contraseña correcta");
+       } else if (usuarioSesion != nameUser && passwordSesion != passwordUser) {
+           alert("Por favor ingresa correctamente tus datos")
+       } else {
+           "Vuelve pronto.";
+       }
+       continuar = confirm("¿Quieres ver nuevamente el catálogo?")
+   
+   } while (continuar == true);
 } else {
-    alert ("Lo sentimos " + nameUser +  " debes de tener mayoría de edad para realizar compras. Pídele a tus papás que realicen la compra por ti");
+    alert ("Lo sentimos " + nameUser +  " debes de tener mayoría de edad para realizar compras. Pídele a tus papás o a alguna persona mayor que realicen la compra por ti. Nos vemos, pilluelo.");
 }
 
-let continuar = confirm (nameUser + " ¿Quieres conocer los artículos de nuestro catálogo?");
 
-
-do {
-     const usuarioSesion = prompt ("Por favor ingresa tu nombre de usuario");
-    const passwordSesion = prompt ("Por favor ingresa tu contraseña");
-
-    if (usuarioSesion == nameUser && passwordSesion == passwordUser) {
-        alert ("Bienvenido " + nameUser + " por el momento tenemos a la venta los siguientes productos: " + item1 + ", " + item2 + ", "+ item3 + ", "+ item4 + ".");
-    } else if (usuarioSesion != nameUser && passwordSesion == passwordUser) {
-        alert ("Por favor, ingresa un nombre de usuario correcto");
-    } else if (usuarioSesion == nameUser && passwordSesion != passwordUser){
-        alert("Por favor, ingresa tu contraseña correcta");
-    } else if (usuarioSesion != nameUser && passwordSesion != passwordUser) {
-        alert("Por favor ingresa correctamente tus datos")
-    } else {
-        "Vuelve pronto.";
-    }
-
-} while (continuar != false);
 
